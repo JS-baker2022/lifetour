d1f98f52b072c2012a37d59a458569016aa06113
booblik2022 <booblik2022@gmail.com>
2024-12-18 22:46:36 +0300

    Добавил логику работы кнопки меню

--- source/js/main.js
+++ source/js/main.js
@@ -2,3 +2,11 @@
 // import Swiper from "swiper";
 // import {Navigation, Pagination} from "swiper/modules";
 // import 'swiper/css';
+
+const menu = document.querySelector('.header__wrapper');
+const menuButton = document.querySelector('.header__menu-button');
+
+menuButton.onclick = function() {
+  menu.classList.toggle('header__wrapper--is-open');
+  menu.classList.toggle('header__wrapper--is-close');
+};
